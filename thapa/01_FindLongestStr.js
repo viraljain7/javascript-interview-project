fce22191767573bf8ed51ae920bef81ba6b52e77
viraljain7 <veerljain1234@gmail.com>
2024-09-03 23:59:10 +0530

    longStr Que comment add

--- thapa/01_FindLongestStr.js
+++ thapa/01_FindLongestStr.js
@@ -1,11 +1,17 @@
-let str = "hello my name is viral jain, username viraljain7 and you are great"
+let str = "hello my name is viral jain, username viraljain7 and you are great";
 
-const findLongestStr = (str) => {
+const findLongestWord = (str) => {
+    // Check if the string is empty or contains only whitespace
     if (str.trim().length === 0) return false;
 
-    words = str.split(" ")
-    words = words.sort((a, b) => b.length - a.length)
-    return words[0]
+    // Split the string into an array of words using a space as a delimiter
+    let words = str.split(" ");
+
+    // Sort the array of words based on the length of each word, in descending order
+    words = words.sort((a, b) => b.length - a.length);
+
+    // Return the first word in the sorted array, which is the longest word
+    return words[0];
 }
 
-console.log(findLongestStr(str));
+console.log(findLongestWord(str)); // Output: "viraljain7"
